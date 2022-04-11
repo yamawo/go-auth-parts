5d7607ccc46dc1d7a7ff01cd1c1a8201d3fb9d8a
yamawo <y7a3m1asaki@gmail.com>
2022-04-12 00:44:34 +0900

    feat: driver

---
+++ driver/user.go
@@ -0,0 +1,48 @@
+package driver
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"auth-parts/adapter/controller"
+	"auth-parts/adapter/gateway"
+	"auth-parts/adapter/presenter"
+	"auth-parts/usecase/interactor"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// server を立てる
+func Serve(addr string) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DATABASE"),
+	)
+
+	conn, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	user := controller.User{
+		OutputFactory: presenter.NewUserOutputPort,
+		InputFactory:  interactor.NewUserInputPort,
+		RepoFactory:   gateway.NewUserRepository,
+		Conn:          conn,
+	}
+
+	http.HandleFunc("/user/", user.GetUserByID)
+	err = http.ListenAndServe(addr, nil)
+
+	if err != nil {
+		log.Fatalf("Listen and serve failed. %+v", err)
+	}
+}
